c5785b426bfc1b8474db371ff73f3184e9e8798e
yurii <sllouyssgort@gmail.com>
2023-04-11 15:44:51 +0200

    Refactoring

--- contracts/VelGeekToken.sol
+++ contracts/VelGeekToken.sol
@@ -6,22 +6,17 @@ import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract VelGeekToken is ERC20, Ownable {
     uint supply = 100000;
-    uint minted = 0;
 
-    constructor() ERC20("VelGeek", "VGK") {
+    constructor() ERC20("VelGeek", "VELGEEK") {
         _mint(msg.sender, supply * 10 ** decimals());
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
+    function _transfer(address from, address to, uint256 amount)
     internal virtual override
     {
-        if (minted == 0) {
-            minted = supply;
-        } else {
-            require(from == owner() || balanceOf(to) + amount <= 1000 * 10 ** decimals(),
-                "Balance will reach its limit of 1000 tokens when it comes to the address");
-        }
+        require(from == owner() || balanceOf(to) + amount <= 1000 * 10 ** decimals(),
+            "Balance will reach its limit of 1000 tokens when it comes to the address");
 
-        super._beforeTokenTransfer(from, to, amount);
+        super._transfer(from, to, amount);
     }
 }
