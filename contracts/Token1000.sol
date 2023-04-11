2c45edd6bf08b9294d90c38c8f42986b8adfb7ce
YAMISHKA02 <yashkur.misha@gmail.com>
2023-04-11 14:32:37 +0300

    fixed functionality

--- contracts/Token1000.sol
+++ contracts/Token1000.sol
@@ -1,21 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-
 import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, Ownable {
-    constructor() ERC20("MyToken", "MTK") {}
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+contract VeelGeekERC20 is ERC20, Ownable {
+    uint supply = 100000;
+    uint minted = 0;
+    constructor() ERC20("VelGeek", "VGK") {
+        _mint(msg.sender, supply * 10 ** decimals());
     }
     function _beforeTokenTransfer(address from, address to, uint256 amount)
         internal virtual override
         {
             super._beforeTokenTransfer(from, to, amount);
-
-            require(balanceOf(to) <= 1000 * 10 ** decimals(),"More 1000 tokens on address");
-            require(balanceOf(to) + amount <= 1000 * 10 ** decimals(),"More 1000 tokens on address after transaction");
+            if(minted == 0){
+                minted = supply;
+            }else{
+                require(balanceOf(to) <= 1000 * 10 ** decimals(),"More 1000 tokens on address");
+                require(balanceOf(to) + amount <= 1000 * 10 ** decimals(),"More 1000 tokens on address after transaction");
+            }
         }
 }
--- test/tokenTest.ts
+++ test/tokenTest.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
-import { MyToken } from "../typechain-types";
+import { VeelGeekERC20 } from "../typechain-types";
 import {BigNumber} from "ethers";
 
 
@@ -9,13 +9,14 @@ let deployer: SignerWithAddress
 let user1: SignerWithAddress
 let user2: SignerWithAddress
 let Token
-let token: MyToken
+let token: VeelGeekERC20
 let decimals: BigNumber
+let allowance = 100000
 decimals = BigNumber.from(10).pow(18)
 describe("Token testing", async()=>{
     before(async()=>{
         [deployer, user1,user2] = await ethers.getSigners()
-        Token = await ethers.getContractFactory("MyToken")
+        Token = await ethers.getContractFactory("VeelGeekERC20")
         token = await Token.deploy()
         await token.deployed()
     })
@@ -25,8 +26,8 @@ describe("Token testing", async()=>{
         it("Should to be correct owner", async()=>{
             expect(await token.owner()).equal(deployer.address)
         })
-        it("User balances equal 0", async()=>{
-            expect(await token.balanceOf(deployer.address)).deep.equal(0)
+        it("Owner balance equal 100000 tokens", async()=>{
+            expect(await token.balanceOf(deployer.address)).deep.equal(decimals.mul(allowance))
             expect(await token.balanceOf(user1.address)).deep.equal(0)
             expect(await token.balanceOf(user2.address)).deep.equal(0)
         })
@@ -34,35 +35,24 @@ describe("Token testing", async()=>{
     describe("Basic operations", async()=>{
         before(async()=>{
             [deployer, user1,user2] = await ethers.getSigners()
-            Token = await ethers.getContractFactory("MyToken")
+            Token = await ethers.getContractFactory("VeelGeekERC20")
             token = await Token.deploy()
             await token.deployed()
         })
-        it("Should to be mintable by owner", async()=>{
-            await token.mint(user1.address, decimals.mul(100))
-            await token.mint(deployer.address, decimals.mul(1000))
-            expect(await token.balanceOf(user1.address)).equals(decimals.mul(100))
-            expect(await token.balanceOf(deployer.address)).equals(decimals.mul(1000))
-        })
         it("Should to can be transfered", async()=>{
-            await expect(() => token.transfer(user1.address, 100))
-                .to.changeTokenBalances(token, [deployer.address, user1.address], [-100, 100]);
-            await expect(() => token.connect(user1).transfer(user2.address, 100))
-                .to.changeTokenBalances(token, [ user1.address, user2.address], [-100, 100]);
-            await expect(() => token.connect(user2).transfer(deployer.address, 100))
-                .to.changeTokenBalances(token, [ user2.address, deployer.address], [-100, 100]);  
+            await expect(() => token.transfer(user1.address, 100000))
+                .to.changeTokenBalances(token, [deployer.address, user1.address], [-100000, 100000]);
+            
         })
         
     })
     describe("Check 1000 tokens limit by account", async()=>{
-        it("Cant be minted if limit reaches", async()=>{
-            await expect(token.mint(user1.address, decimals.mul(1001))).to.be.revertedWith('More 1000 tokens on address after transaction')
-        })
         it("Cant be transfered if limit reaches", async()=>{
             let balanceOwner = await token.balanceOf(deployer.address)
             let balanceUser1 = await token.balanceOf(user1.address)
-            expect((balanceOwner.add(balanceUser1)).gt(decimals.mul(1000)))
-            await expect(token.transfer(user1.address, decimals.mul(1001))).to.be.revertedWith('More 1000 tokens on address after transaction')
+            let allBalance = balanceOwner.add(balanceUser1)
+            expect(allBalance).gt(decimals.mul(1000))
+            await expect(token.transfer(user1.address, allBalance)).to.be.revertedWith('More 1000 tokens on address after transaction')
         })
     })
 })
