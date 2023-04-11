32f745945a7a83cf9ff619960dc17cfaa1592f7e
YAMISHKA02 <yashkur.misha@gmail.com>
2023-04-11 14:36:09 +0300

    deleted

--- scripts/01_deployToken.ts
+++
@@ -1,9 +0,0 @@
-import { ethers } from "hardhat"
-import { DeployFunction } from "hardhat-deploy/types"
-
-const deploy: DeployFunction = async()=>{
-  const Token = await ethers.getContractFactory("MyContract");
-  const token = await Token.deploy();
-  await token.deployed();
-  console.log("MyContract deployed to:", token.address);
-}
